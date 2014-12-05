4d6356e65cf92e74809efc022090e688204a41b5
Jagan <jagan262gmail.com>
2014-12-05 14:26:29 +0000

    working now

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,12 +1,14 @@
 class UploadsController < ApplicationController
   def index
 
+
   end
 
   	 def upload
   uploaded_io = params[:dataf]
   File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
     file.write(uploaded_io.read)
+    
 end
     flash[:notice] = "File has been uploaded successfully"
     redirect_to uploads_index_path
